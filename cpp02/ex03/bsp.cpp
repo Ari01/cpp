0a0a5402cc49fc0616f9c0045f2a111614c6ac98
denis <denis.c1@live.fr>
2022-02-14 10:40:00 +0100

    correction 02

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,36 +6,11 @@
 /*   By: dchheang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 22:19:23 by dchheang          #+#    #+#             */
-/*   Updated: 2022/02/14 10:24:58 by dchheang         ###   ########.fr       */
+/*   Updated: 2022/02/14 10:39:26 by dchheang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-
-/*Point	vector_sub(Point v1, Point v2)
-{
-	Fixed	x;
-	Fixed	y;
-
-	x = v1.getx() - v2.getx();
-	y = v1.gety() - v2.gety();
-	return (Point(x,y));
-}
-
-Fixed	vector_cross(Point v1, Point v2)
-{
-	return (v1.getx() * v2.gety() - v1.gety() * v2.getx());
-}
-
-bool	is_left(Point v1, Point v2, Point p)
-{
-	Point	edge;
-	Point	vp;
-
-	edge = vector_sub(v1, v2);
-	vp = vector_sub(p, v2);
-	return (vector_cross(edge, vp) < 0);
-}*/
 
 Fixed	sign(Point p1, Point p2, Point p3)
 {
